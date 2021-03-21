20bda1dec81f2be81149ad6ce7c48af0d8bc1266
Maxime HAMM <maxime.hamm@laposte.net>
2021-03-21 23:59:11 +0100

    Export to pdf

--- plugin/src/main/kotlin/io/nimbly/tzatziki/actions/TzExportAction.kt
+++ plugin/src/main/kotlin/io/nimbly/tzatziki/actions/TzExportAction.kt
@@ -10,6 +10,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiRecursiveVisitor
 import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.util.elementType
 import com.intellij.testFramework.writeChild
 import io.nimbly.tzatziki.pdf.*
 import io.nimbly.tzatziki.psi.getModule
@@ -23,6 +24,11 @@ import java.io.ByteArrayOutputStream
 class TzExportAction : TzAction(), DumbAware {
 
     override fun actionPerformed(event: AnActionEvent) {
+
+        //TODO : Ma,age Cucumber "Rule" sections
+        //TODO : Traduire les mots clef au moment d'editer
+        //TODO : Manage tril quote ?
+        //TODO : Manage comments
 
         val file = event.getData(CommonDataKeys.PSI_FILE) ?: return
         val project = event.getData(CommonDataKeys.PROJECT) ?: return
@@ -40,25 +46,32 @@ class TzExportAction : TzAction(), DumbAware {
         // Customize styles
         generator.style.contentStyle = //language=CSS
             """
+                * { font-size: 16px; margin: 0 0 0 0 }
                 p { margin: 0 }
                 
-                h1 { font-size: 30px; margin-bottom: 10px; } 
-                h2 { font-size: 25px; border-bottom: 5px; } 
+                h1 { font-size: 24px; margin-bottom: 10px; } 
+                h2 { font-size: 20px; border-bottom: 5px; } 
                 
-                table { margin-top: 20px; }
+                table { margin-top: 10px; max-width: 100%; margin-left: 15px;}
                 table, th, td {  
-                    font-size: 25px;
+                    font-size: 14px; 
                     vertical-align: top; 
                     border: 1px solid midnightblue;  
                     border-collapse: collapse;  
                 }  
-                th, td { padding: 20px; }
+                th, td { padding: 5px;  white-space: nowrap; }
+                th { color: chocolate }
+                
+                div { display: inline-block; }
                 
                 .feature { margin-left: 5px; }
                 .featureHeader { margin-left: 5px; font-weight: bolder }
-                .scenario, .scenarioOutline { margin-left: 20px; }
-                .step { margin-left: 40px; }
-                .examples { margin-left: 40px; }
+                .scenario, .scenarioOutline { margin-left: 15px; }
+                .step { margin-left: 20px; }
+                .stepParameter { color: chocolate; font-weight: bolder }
+                .examples { margin-left: 20px; }
+                
+                .stepKeyword { color: grey; }
                 
                 """.trimIndent()
 
@@ -120,10 +133,12 @@ class TzExportAction : TzAction(), DumbAware {
         }
 
         override fun visitScenario(scenario: GherkinScenario?) {
+            generator.paragraphStarts()
             openParagrah("scenario")
             openTag("h2")
             super.visitScenario(scenario)
             closeParagraph()
+            generator.paragraphEnds()
         }
 
         override fun visitStep(step: GherkinStep?) {
@@ -132,16 +147,26 @@ class TzExportAction : TzAction(), DumbAware {
             closeParagraph()
         }
 
+        override fun visitStepParameter(gherkinStepParameter: GherkinStepParameterImpl?) {
+            openClass("stepParameter")
+            super.visitStepParameter(gherkinStepParameter)
+            closeClass()
+        }
+
         override fun visitExamplesBlock(block: GherkinExamplesBlockImpl?) {
+            generator.paragraphStarts()
             openParagrah("examples")
             super.visitExamplesBlock(block)
             closeParagraph()
+            generator.paragraphEnds()
         }
 
         override fun visitTable(table: GherkinTableImpl) {
+            generator.paragraphStarts()
             openTag("table")
             super.visitTable(table)
             closeTag()
+            generator.paragraphEnds()
         }
 
         override fun visitTableHeaderRow(row: GherkinTableHeaderRowImpl) {
@@ -163,12 +188,25 @@ class TzExportAction : TzAction(), DumbAware {
         }
 
         override fun visitElement(element: PsiElement) {
+
             ProgressIndicatorProvider.checkCanceled()
-            if (element is LeafPsiElement) {
-                if (!(context.isRow() && element.text == "|"))
+
+            fun append() {
+                if (element !is LeafPsiElement) return
+                if (context.isRow() && element.text == "|") return
+                if (element.elementType == GherkinTokenTypes.STEP_PARAMETER_BRACE) return
+
+                if (element.elementType == GherkinTokenTypes.STEP_KEYWORD) {
+                    openClass("stepKeyword")
                     append(element.text)
+                    closeClass()
+                }
+                else {
+                    append(element.text)
+                }
             }
 
+            append()
             context.push(element)
             element.acceptChildren(this)
             context.pop()
@@ -177,6 +215,12 @@ class TzExportAction : TzAction(), DumbAware {
         private fun openTag(tag: String): TzatizkiVisitor {
             generator.append("<$tag>")
             stackTags.push(tag)
+            return this
+        }
+
+        private fun openClass(clazz: String): TzatizkiVisitor {
+            generator.append("<div class='$clazz'>")
+            stackTags.push("div")
             return this
         }
 
@@ -192,6 +236,8 @@ class TzExportAction : TzAction(), DumbAware {
         }
 
         private fun closeParagraph() = closeTag()
+
+        private fun closeClass() = closeTag()
 
         private fun append(string: String): TzatizkiVisitor {
             generator.append(string.escape())
