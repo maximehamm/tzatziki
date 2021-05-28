be34d831ef7db16d38df1446215a36de2b0d809b
Maxime HAMM <maxime.hamm@laposte.net>
2021-05-28 17:52:04 +0200

    Strike out deprecated steps inspection - Javascript

---
+++ extensions/javascript/src/main/kotlin/io/nimbly/tzatziki/JavascriptTzatzikiExtensionPoint.kt
@@ -0,0 +1,42 @@
+/*
+ * CUCUMBER +
+ * Copyright (C) 2021  Maxime HAMM - NIMBLY CONSULTING - maxime.hamm.pro@gmail.com
+ *
+ * This document is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This work is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package io.nimbly.tzatziki
+
+import com.intellij.lang.javascript.psi.*
+import com.intellij.psi.PsiElement
+
+class JavascriptTzatzikiExtensionPoint : TzatzikiExtensionPoint {
+
+    override fun isDeprecated(element: PsiElement): Boolean {
+        if (element !is JSLiteralExpression)
+            return false
+
+        val args = element.parent
+        if (args !is JSArgumentList)
+            return false
+
+        val fct = args.arguments.filterIsInstance<JSFunctionExpression>().firstOrNull()
+        if (fct != null)
+            return fct.isDeprecated
+
+        val ref = args.arguments.filterIsInstance<JSReferenceExpression>().firstOrNull()?.resolve()
+            ?: return false
+        if (ref is JSFunction)
+            return ref.isDeprecated
+
+        return false
+    }
+}
