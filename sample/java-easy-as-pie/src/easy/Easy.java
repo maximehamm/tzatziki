615208b7c003706506a286a19e7f7d264f9db29a
Maxime HAMM <maxime.hamm@laposte.net>
2021-05-29 22:17:14 +0200

    Sample - java easy as pie

---
+++ sample/java-easy-as-pie/src/easy/Easy.java
@@ -0,0 +1,12 @@
+package easy;
+public class Easy {
+    @io.cucumber.java.en.Given("This is given")
+    public void thisIsGiven() {
+    }
+    /**
+     * @deprecated xxx
+     */
+    @io.cucumber.java.en.Then("This is easy as pie")
+    public void thisIsEasyAsPie() {
+    }
+}
