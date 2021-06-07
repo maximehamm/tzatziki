4e69890729d69ec4460d085a3b8b70a3b18e3e51
Maxime HAMM <maxime.hamm@laposte.net>
2021-06-07 17:58:41 +0200

    Examples

---
+++ sample/javascript-easy-as-pie/features/base.feature
@@ -0,0 +1,4 @@
+Feature: Searching for TestCafe on GitHub
+  I want to find TestCafe repository on GitHub
+  Scenario: Searching for TestCafe on GitHub
+    Given This is my test
---
+++ sample/javascript-easy-as-pie/features/step_definitions/google.js
@@ -0,0 +1,31 @@
+var { defineSupportCode } = require('cucumber');
+var Selector       = require('testcafe').Selector;
+
+defineSupportCode(function ({ Given, When, Then }) {
+    var testController = null;
+
+    Given('I am open Google\'s search page', function () {
+        return this.waitForTestController()
+            .then(function (tc) {
+                testController = tc;
+
+                return testController.navigateTo('http://google.com');
+            });
+    });
+
+    When('I am typing my search request "([^"]*)" on Google', function (text) {
+        var input = Selector('#lst-ib').with({ boundTestRun: testController });
+
+        return testController.typeText(input, text);
+    });
+
+    Then('I am pressing "([^"]*)" key on Google', function (text) {
+        return testController.pressKey(text);
+    });
+
+    Then('I should see that the first Google\'s result is "([^"]*)"', function (text) {
+        var firstLink = Selector('#rso').find('a').with({ boundTestRun: testController });
+
+        return testController.expect(firstLink.innerText).contains(text);
+    });
+});
