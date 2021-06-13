97683357816724cca320912a64b4daeac71e9be2
Maxime HAMM <maxime.hamm@laposte.net>
2021-06-13 21:47:04 +0200

    Line breakpoints - scala

--- extensions/scala/src/main/kotlin/io/nimbly/tzatziki/ScalaTzatzikiExtensionPoint.kt
+++ extensions/scala/src/main/kotlin/io/nimbly/tzatziki/ScalaTzatzikiExtensionPoint.kt
@@ -67,8 +67,92 @@ class ScalaTzatzikiExtensionPoint : TzatzikiExtensionPoint {
     }
 
     override fun findBreakpoint(source: PsiElement, stepDefinitions: List<AbstractStepDefinition>): TzBreakpoint? {
+
         return null
+//        val methods = stepDefinitions
+//            .mapNotNull { it.element }
+//            .filterIsInstance<ScMethodCall>()
+//
+//        if (methods.isEmpty())
+//            return null
+//
+//        val breakpoints = DebuggerManagerEx.getInstanceEx(methods.first().project)
+//            .breakpointManager
+//            .breakpoints
+//            .filter { it.xBreakpoint.sourcePosition?.offset != null }
+//
+//        var tzbreakpoint: TzBreakpoint? = null
+//        methods
+//            .filter { it.getDocument() != null }
+//            .filter { it.getDocumentLine() != null }
+//            .forEach { method ->
+//
+//                breakpoints
+//                    .filter { method.parent.textRange.contains(it.xBreakpoint.sourcePosition?.offset ?: -1) }
+//                    .forEach { breakpoint ->
+//
+//                        val tooltip = breakpoint.displayName
+//                        val navigatable = breakpoint.xBreakpoint.navigatable ?: method.navigationElement as Navigatable
+//                        val icon =
+//                            if (breakpoint.xBreakpoint.isEnabled) breakpoint.xBreakpoint.type.enabledIcon
+//                            else breakpoint.xBreakpoint.type.disabledIcon
+//
+//                        tzbreakpoint = TzBreakpoint(navigatable, tooltip, icon,
+//                            listOfNotNull(source, method, breakpoint.evaluationElement))
+//
+//                        if (breakpoint.xBreakpoint.isEnabled)
+//                            return tzbreakpoint
+//                    }
+//            }
+//
+//        return tzbreakpoint
     }
 
-    override fun initBreakpointListener(project: Project) = Unit
+    override fun initBreakpointListener(project: Project) {
+
+//        fun refresh(breakpoint: XBreakpoint<*>) {
+//
+//            val sourcePosition = breakpoint.sourcePosition ?: return
+//
+//            val vfile = sourcePosition.file
+//            if (!vfile.isValid) return
+//            val file = vfile.getFile(project) ?: return
+//            if (file !is ScalaFile) return
+//
+//            val element = file.findElementAt(sourcePosition.offset) ?: return
+//            val method = PsiTreeUtil.getParentOfType(element, ScMethodCall::class.java) ?: return
+//
+//            val usagesManager = (FindManager.getInstance(project) as FindManagerImpl).findUsagesManager
+//            val handler = usagesManager.getFindUsagesHandler(method, false) ?: return
+//
+//            val references = mutableListOf<PsiReference>()
+//            handler.processElementUsages(method.parent, {
+//                val ref = it.reference
+//                if (ref !=null)
+//                    references.add(ref)
+//                true
+//            }, handler.findUsagesOptions)
+//
+//            references
+//                .asSequence()
+//                .filterIsInstance<CucumberStepReference>()
+//                .map { it.element }
+//                .filterIsInstance<GherkinStep>()
+//                .map { it.containingFile }
+//                .toSet()
+//                .forEach {
+//                    DaemonCodeAnalyzer.getInstance(project).restart(it)
+//                }
+//
+//        }
+//
+//        project.messageBus
+//            .connect()
+//            .subscribe(XBreakpointListener.TOPIC, object : XBreakpointListener<XBreakpoint<*>> {
+//                override fun breakpointChanged(breakpoint: XBreakpoint<*>) = refresh(breakpoint)
+//                override fun breakpointAdded(breakpoint: XBreakpoint<*>) = refresh(breakpoint)
+//                override fun breakpointRemoved(breakpoint: XBreakpoint<*>) = refresh(breakpoint)
+//                override fun breakpointPresentationUpdated(breakpoint: XBreakpoint<*>, session: XDebugSession?) = Unit
+//            })
+    }
 }
