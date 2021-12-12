9d3b43a898545715233202f58a53c211a8c706a5
Maxime HAMM <maxime.hamm@laposte.net>
2021-12-12 23:44:43 +0100

    Tags tool view

--- plugin/src/main/kotlin/io/nimbly/tzatziki/view/CucumberPlusTagsView.kt
+++ plugin/src/main/kotlin/io/nimbly/tzatziki/view/CucumberPlusTagsView.kt
@@ -24,10 +24,13 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanelWithEmptyText
+import com.intellij.ui.components.JBTextArea
+import com.intellij.uiDesigner.core.GridConstraints
 import io.nimbly.tzatziki.psi.getGherkinScope
 import io.nimbly.tzatziki.util.findAllTags
 import java.awt.BorderLayout
 import java.awt.FlowLayout
+import java.awt.GridLayout
 import javax.swing.JPanel
 import javax.swing.border.EmptyBorder
 
@@ -45,11 +48,10 @@ class CucumberPlusTagsView(private val project: Project)
         p.border = EmptyBorder(10, 10, 10, 10)
         p.withEmptyText("No tags found")
 
-        val title = JBLabel("""<html>
-            Only the scenarios with the selected tags will be triggered.<br/><br/>
-            <b>Select Tags</b> :
-            </html>""".trimMargin())
-        p.add(title, BorderLayout.PAGE_START)
+        p.add(JBLabel("""<html>
+                Only the scenarios with the selected tags will be triggered.<br/><br/>
+                <b>Select Tags</b> :
+                </html>""".trimMargin()), BorderLayout.PAGE_START)
 
         lateinit var tagsPanel: JPanel
 
@@ -85,15 +87,35 @@ class CucumberPlusTagsView(private val project: Project)
 
         val tags = findAllTags(project, project.getGherkinScope()).sortedBy { it.tag.name.toUpperCase() }
 
-        val p = JBPanelWithEmptyText()
-        p.layout = FlowLayout(FlowLayout.LEFT)
+        // Selection
+        val tSelection = JBTextArea(4, 10)
+        val pSelection = JBPanelWithEmptyText(BorderLayout())
+        pSelection.add(JBLabel("""<html>
+                <b>Selection</b> :<br/>
+                </html>""".trimMargin()), BorderLayout.NORTH)
+        pSelection.add(tSelection, BorderLayout.CENTER);
 
+        // Tags
+        val pTags = JBPanelWithEmptyText(FlowLayout(FlowLayout.LEFT))
         tags.forEach { tag ->
             val t = JBCheckBox(tag.tag.name)
-            p.add(t)
+            pTags.add(t)
         }
 
-        return p
+        // Main
+        val main = JBPanelWithEmptyText(GridLayout(2, 1))
+        main.add(pTags, GridConstraints(
+            0, 0, 1, 1,
+            GridConstraints.ANCHOR_NORTH, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW,
+            null, null, null))
+        main.add(pSelection, GridConstraints(
+            1, 0, 1, 1,
+            GridConstraints.ANCHOR_NORTH, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, null, null))
+
+        return main
     }
 
 }
