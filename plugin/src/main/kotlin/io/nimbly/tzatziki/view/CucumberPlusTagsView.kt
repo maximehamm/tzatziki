b52e7f369b89d0de6373279662c6bc72c38f707c
Maxime HAMM <maxime.hamm@laposte.net>
2021-12-13 10:48:24 +0100

    Tags tool view

--- plugin/src/main/kotlin/io/nimbly/tzatziki/view/CucumberPlusTagsView.kt
+++ plugin/src/main/kotlin/io/nimbly/tzatziki/view/CucumberPlusTagsView.kt
@@ -24,13 +24,14 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.ui.components.*
 import com.intellij.uiDesigner.core.GridConstraints
 import com.intellij.uiDesigner.core.GridLayoutManager
+import com.intellij.util.ui.WrapLayout
 import io.nimbly.tzatziki.psi.getGherkinScope
 import io.nimbly.tzatziki.util.findAllTags
 import java.awt.BorderLayout
 import java.awt.FlowLayout
 import javax.swing.JPanel
 import javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
-import javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
+import javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED
 import javax.swing.border.EmptyBorder
 
 class CucumberPlusTagsView(private val project: Project)
@@ -90,37 +91,35 @@ class CucumberPlusTagsView(private val project: Project)
         val tSelection = JBTextArea(4, 10)
 
         // Tags
-        val pTags = JBPanelWithEmptyText(FlowLayout(FlowLayout.LEFT))
+        val pTags = JBPanelWithEmptyText(WrapLayout(FlowLayout.LEFT))
         (tags.toList() + tags.toList() + tags.toList())
             .forEach { tag ->
             val t = JBCheckBox(tag.tag.name)
             pTags.add(t)
         }
-        val sTags = JBScrollPane(pTags)
-        sTags.horizontalScrollBarPolicy = HORIZONTAL_SCROLLBAR_NEVER
-        sTags.verticalScrollBarPolicy = VERTICAL_SCROLLBAR_ALWAYS
+        val sTags = JBScrollPane(pTags, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER)
 
         // Main
         val main = JBPanelWithEmptyText(GridLayoutManager(3, 1))
         main.add(sTags, GridConstraints(
             0, 0, 1, 1,
-            GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_VERTICAL,
+            GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
             GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW or GridConstraints.SIZEPOLICY_WANT_GROW,
             GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW or GridConstraints.SIZEPOLICY_WANT_GROW,
-            null, null, null).also { isVertical = false } )
+            null, null, null))
 
         main.add(JBLabel("""<html>
                 <b>Selection</b> :<br/><br/>
                 </html>""".trimMargin()), GridConstraints(
             1, 0, 1, 1,
             GridConstraints.ANCHOR_SOUTHWEST, GridConstraints.FILL_NONE,
-            GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK, GridConstraints.SIZEPOLICY_FIXED,
             null, null, null))
 
         main.add(tSelection, GridConstraints(
             2, 0, 1, 1,
             GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_HORIZONTAL,
-            GridConstraints.SIZEPOLICY_CAN_SHRINK, GridConstraints.SIZEPOLICY_CAN_SHRINK,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK, GridConstraints.SIZEPOLICY_FIXED,
             null, null, null))
 
         return main
