90a253718ed5c6dcdee3fc32150fb869e37cfe68
Maxime HAMM <maxime.hamm@laposte.net>
2023-03-08 15:23:08 +0100

    Generate step definition

--- extensions/java-cucumber/src/main/kotlin/io/nimbly/tzatziki/generation/TzGherkinJavaExtension.kt
+++ extensions/java-cucumber/src/main/kotlin/io/nimbly/tzatziki/generation/TzGherkinJavaExtension.kt
@@ -3,17 +3,22 @@ package io.nimbly.tzatziki.generation
 import com.intellij.psi.PsiFile
 import org.jetbrains.plugins.cucumber.StepDefinitionCreator
 import org.jetbrains.plugins.cucumber.java.CucumberJavaExtension
-import org.jetbrains.plugins.cucumber.java.steps.JavaStepDefinitionCreator
+import org.jetbrains.plugins.cucumber.java.steps.TzJavaStepDefinitionCreator
 import org.jetbrains.plugins.cucumber.psi.GherkinStep
 import org.jetbrains.plugins.cucumber.psi.impl.GherkinStepImpl
 
 class TzGherkinJavaExtension : CucumberJavaExtension() {
 
     override fun getStepDefinitionCreator(): StepDefinitionCreator {
-        return TzcreateStepDefinition()
+        return TzcreateStepDefinitionTz()
     }
 
-    private class TzcreateStepDefinition : JavaStepDefinitionCreator() {
+    private class TzcreateStepDefinitionTz : TzJavaStepDefinitionCreator() {
+//        override fun createStepDefinition(step: GherkinStep, file: PsiFile, withTemplate: Boolean): Boolean {
+//            val stepX = if (step is GherkinStepImpl) TzGherkinStep(step) else step
+//            return super.createStepDefinition(stepX, file, withTemplate)
+//        }
+
         override fun createStepDefinition(step: GherkinStep, file: PsiFile, withTemplate: Boolean): Boolean {
             val stepX = if (step is GherkinStepImpl) TzGherkinStep(step) else step
             return super.createStepDefinition(stepX, file, withTemplate)
